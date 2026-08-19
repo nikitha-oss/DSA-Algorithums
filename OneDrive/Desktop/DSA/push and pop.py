badf02aeb0ed2866dc54463c3b657ce45cbe2788
nikitha-oss <nikitha1653@gmail.com>
2026-08-19 11:48:04 +0530

    DSA

---
+++ OneDrive/Desktop/DSA/push and pop.py
@@ -0,0 +1,12 @@
+stack = []
+
+stack.append(10)
+stack.append(20)
+stack.append(30)
+
+print("Before pop:", stack)
+
+item = stack.pop()
+
+print("Popped element:", item)
+print("After pop:", stack)
